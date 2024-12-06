54adbf739280a84d9b9a1a824b76f0ffc4bd014a
F1etcher7 <buzukuckii39@gmail.com>
2024-12-06 23:46:51 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,9 @@
+immutable_var = tuple ([1, 2, 3, 4])
+print(immutable_var)
+
+#Кортеж - неизменяемая упорядоченная коллекция, которая может содержать в себе разные типы данных.
+#Следовательно кортеж изменять нельзя.
+
+mutable_list =  [1, 2, 3, 4, 5, 6]
+mutable_list.append('Vladislav')
+print(mutable_list)
